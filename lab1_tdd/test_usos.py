15f3686da8eedfadf2bb60ce878e1dbf4a32cbb3
Ol3hhh <olehmaru@gmail.com>
2026-03-19 16:44:56 +0100

    add usos test implementation

---
+++ lab1_tdd/test_usos.py
@@ -0,0 +1,40 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+import time
+
+class InputTesting(unittest.TestCase):
+    BLOG_URL = "https://web.usos.pwr.edu.pl/kontroler.php?_action=logowaniecas/index&callback=g7YyNrVS0s%2F0zyspys9JLdIsryCiwj09MLsnMz7PNSy0v1k9JTUsszS1RsgYAa3d6d19f42e41bfa7f4bcd2d8d3b56d4b3a9d247"
+    INPUT_NAME = "username"
+
+    def setUp(self):
+        options = Options()
+        options.binary_location = '/snap/firefox/current/usr/lib/firefox/firefox'
+        self.driver = webdriver.Firefox(options=options)
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_input_value_and_clear_button(self):
+        self.driver.get(self.BLOG_URL)
+        try:
+            login_box = self.driver.find_element(By.NAME, self.INPUT_NAME)
+        except Exception:
+            self.fail("Login input not found!")
+
+        login_box.send_keys("testowy_login")
+        inputValue = login_box.get_attribute("value")
+        self.assertEqual("testowy_login", inputValue)
+
+        try:
+            clear_button = self.driver.find_element(By.XPATH, "//*[@type='reset' or @name='clear' or contains(text(), 'Wyczyść')]")
+            clear_button.click()
+        except Exception:
+            self.fail("Przycisk 'Wyczyść' nie został znaleziony na stronie!")
+
+        clearedValue = login_box.get_attribute("value")
+        self.assertEqual("", clearedValue, "Błąd: Pole logowania nie zostało wyczyszczone po kliknięciu!")
+
+if __name__ == "__main__":
+    unittest.main() 
